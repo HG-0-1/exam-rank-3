5691420e8aaebda24c65a434a59d716a9e3d6084
Hamdan Alfayez <alfayezh844@gmail.com>
2026-06-21 13:24:17 +0300

    Add function to solve subset sum problem

--- exam2/n_queens/n_queens.c
+++ exam2/n_queens/n_queens.c
@@ -75,3 +75,56 @@ int	main(int ac, char **av)
 	free(board);
 	return (0);
 }
+
+
+
+/////
+#include<unistd.h>
+#include<stdio.h>
+#include <stdlib.h>
+
+void print_sub(int *sub,int size)
+{
+    int i = 0;
+    while(i < size)
+    {
+        if(i)
+            printf(" ");
+        printf("%d",sub[i]);
+        i++;
+    }
+    printf("\n");
+}
+void solve(int *arr,int *sub,int target,int n,int sum,int size,int index)
+{
+    if(index == n)
+    {
+        if(sum == target)
+            print_sub(sub,size);
+        return;
+    }
+    sub[size] = arr[index];
+    solve(arr,sub,target,n,sum + arr[index],size + 1,index + 1);
+    solve(arr,sub,target,n,sum,size,index + 1);
+}
+
+int main(int argc,char **argv)
+{
+    if(argc < 3)
+        return 1;
+    int n = argc -2;
+    int target = atoi(argv[1]);
+    int *arr = malloc(sizeof(int) * n);
+    int *sub = malloc(sizeof(int) * n);
+    if(!arr || !sub)
+        return 1;
+    int i = 0;
+    while(i < n)
+    {
+        arr[i] = atoi(argv[i + 2]);
+        i++;
+    }
+    solve(arr,sub,target,n,0,0,0);
+    free(arr);
+    free(sub);
+}
